2f00ad6c4d22ae69d3d28fcf5330786a39ad4774
AlexPruebas <apathe26@alumnos.uaq.mx>
2024-12-28 16:35:05 -0600

    Se hacen mejoras en el cartdrawer

--- src/app/components/CartDrawer.js
+++ src/app/components/CartDrawer.js
@@ -1,13 +1,15 @@
 // src/components/CartDrawer.js
 'use client';
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Link from "next/link";
 import { CartContext } from "@/context/CartContext"; // Importar el contexto del carrito
 import Image from "next/image"; // Importar la etiqueta Image
 
 export default function CartDrawer({ isOpen, onClose }) {
-  const { cartItems, products, loading, error, removeItemFromCart } = useContext(CartContext);
+  const { cartItems, products, loading, error, removeItemFromCart, addItemToCart } = useContext(CartContext);
+  const [isRemoving, setIsRemoving] = useState(null); // Estado para controlar la eliminación del producto
+  const [isUpdating, setIsUpdating] = useState(null); // Estado para controlar la actualización de cantidad
 
   if (!isOpen) return null;
 
@@ -28,9 +30,35 @@ export default function CartDrawer({ isOpen, onClose }) {
   const shippingProgress = (subtotal >= shippingThreshold ? 100 : (subtotal / shippingThreshold) * 100);
   const shippingFee = subtotal >= shippingThreshold ? 0 : 9.99;
 
+  // Función para manejar la eliminación del producto
+  const handleRemove = async (uniqueID) => {
+    setIsRemoving(uniqueID);
+    await removeItemFromCart(uniqueID);
+    setIsRemoving(null);
+  };
+
+  // Función para manejar el aumento de cantidad
+  const handleAddQuantity = async (item) => {
+    setIsUpdating(item.uniqueID);
+    await addItemToCart({ uniqueID: item.uniqueID, qty: 1 }, false); // Pasar delta +1
+    setIsUpdating(null);
+  };
+
+  // Función para manejar la disminución de cantidad
+  const handleRemoveQuantity = async (item) => {
+    if (item.qty > 1) {
+      setIsUpdating(item.uniqueID);
+      await addItemToCart({ uniqueID: item.uniqueID, qty: -1 }, false); // Pasar delta -1
+      setIsUpdating(null);
+    } else {
+      // Si la cantidad es 1, eliminar el producto del carrito
+      handleRemove(item.uniqueID);
+    }
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-50">
-      <div className="cart-drawer relative bg-white w-full sm:w-3/4 md:w-2/3 lg:w-1/3 p-6 overflow-y-auto">
+      <div className="relative bg-white w-full sm:w-3/4 md:w-2/3 lg:w-1/3 p-6 overflow-y-auto rounded-xl shadow-lg text-[#1c1f28]">
         <button 
           onClick={onClose} 
           className="absolute top-4 right-4 text-2xl text-gray-600 hover:text-gray-800 transition-colors duration-300"
@@ -39,15 +67,33 @@ export default function CartDrawer({ isOpen, onClose }) {
           ❌
         </button>
         
-        <h2 className="text-2xl font-bold mb-4 text-gray-800">Tu carrito</h2>
+        <h2 className="text-2xl font-bold mb-4">Tu carrito</h2>
 
         {loading ? (
           // Skeleton mientras se cargan los productos
           <div>
             {/* Skeleton para el progreso de envío */}
             <div className="mb-4">
-              <div className="h-2 bg-gray-300 rounded-full animate-pulse mt-1"></div>
-              <div className="h-2 bg-gray-300 rounded-full animate-pulse mt-1"></div>
+              <div className="flex justify-between">
+                {subtotal < shippingThreshold ? (
+                  <>
+                    <span className="text-sm text-gray-600">
+                      ${Math.abs(shippingThreshold - subtotal).toFixed(2)} away from free delivery
+                    </span>
+                    <span className="text-sm text-gray-600">
+                      ${(shippingThreshold - subtotal).toFixed(2)} para envío gratis
+                    </span>
+                  </>
+                ) : (
+                  <span className="text-sm text-gray-600">¡Tienes envío gratuito!</span>
+                )}
+              </div>
+              <div className="h-2 bg-gray-200 rounded-full mt-1">
+                <div
+                  className="h-full bg-green-600 rounded-full"
+                  style={{ width: `${shippingProgress}%` }}
+                ></div>
+              </div>
             </div>
 
             {/* Skeleton para los productos */}
@@ -108,31 +154,64 @@ export default function CartDrawer({ isOpen, onClose }) {
 
             {/* Product List */}
             <div className="space-y-4">
-              {detailedCartItems.map((item, index) => (
-                <div key={index} className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                    <Image
-                      src={item.image}
-                      alt={item.name}
-                      width={96} // Ancho de la imagen
-                      height={96} // Alto de la imagen
-                      className="object-cover rounded-md"
-                    />
+              {detailedCartItems.map((item) => (
+                <div key={item.uniqueID} className="flex justify-between items-center">
+                  {/* Información del Producto */}
+                  <div className="flex items-center">
+                    {item.image ? (
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={96} // Ancho de la imagen
+                        height={96} // Alto de la imagen
+                        className="object-cover rounded-md mr-4"
+                      />
+                    ) : (
+                      <div className="w-24 h-24 bg-gray-300 rounded-md mr-4"></div>
+                    )}
                     <div>
                       <Link href={`/product/${item.url}`}>
                         <p className="font-semibold text-gray-800 hover:underline">{item.name}</p>
                       </Link>
-                      {/* Eliminamos la línea de tamaño */}
-                      <p className="text-sm text-gray-600">Cantidad: {item.qty}</p>
+                      <p className="text-sm text-gray-600">Cantidad: {/* Botón para disminuir cantidad */}
+                        <button
+                          onClick={() => handleRemoveQuantity(item)}
+                          className={`px-2 py-1 text-sm bg-transparent text-gray-700 hover:text-black transition-colors duration-200 ${
+                            item.qty === 1 ? 'cursor-not-allowed' : 'cursor-pointer'
+                          }`}
+                          disabled={item.qty === 1 || isUpdating === item.uniqueID}
+                          aria-label={`Disminuir cantidad de ${item.name}`}
+                        >
+                          -
+                        </button>
+                        {/* Cantidad */}
+                        <span className="text-sm text-gray-500">{item.qty}</span>
+                        {/* Botón para aumentar cantidad */}
+                        <button
+                          onClick={() => handleAddQuantity(item)}
+                          className={`px-2 py-1 text-sm bg-transparent text-gray-700 hover:text-black transition-colors duration-200 ${
+                            isUpdating === item.uniqueID ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                          }`}
+                          disabled={isUpdating === item.uniqueID}
+                          aria-label={`Aumentar cantidad de ${item.name}`}
+                        >
+                          +
+                        </button></p>
+                      {/* Controles de Cantidad */}
                     </div>
                   </div>
+                  {/* Controles de Precio y Eliminar */}
                   <div className="flex flex-col items-end">
+                    {/* Precio */}
                     <span className="font-semibold text-lg text-gray-800">${(item.price * item.qty).toFixed(2)}</span>
-                    <button 
-                      className="text-red-500 mt-2 hover:text-red-700"
-                      onClick={() => removeItemFromCart(item.uniqueID)} // Eliminamos `item.size` porque ya no es necesario
+                    {/* Botón de Eliminar */}
+                    <button
+                      onClick={() => handleRemove(item.uniqueID)}
+                      className="text-red-500 hover:text-red-700 mt-2 text-sm"
+                      disabled={isRemoving === item.uniqueID}
+                      aria-label={`Eliminar ${item.name}`}
                     >
-                      Eliminar
+                      {isRemoving === item.uniqueID ? 'Eliminando...' : 'Eliminar'}
                     </button>
                   </div>
                 </div>
@@ -161,11 +240,11 @@ export default function CartDrawer({ isOpen, onClose }) {
             <div className="mt-6 space-y-4">
               <Link href="/checkout">
                 <button className="w-full bg-orange-600 text-white py-3 rounded-lg hover:bg-orange-700 transition-colors duration-300">
-                  Checkout
+                  Pagar
                 </button>
               </Link>
               <Link href="/cart">
-                <button className="w-full bg-gray-200 text-gray-700 py-3 rounded-lg hover:bg-gray-300 transition-colors duration-300">
+                <button className="w-full bg-gray-200 text-gray-700 mt-4 py-3 rounded-lg hover:bg-gray-300 transition-colors duration-300">
                   Ver Carrito
                 </button>
               </Link>
